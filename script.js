16241198fdd52823fa721a4c089d4440a566b93f
Katasravani <105917453+Katasravani@users.noreply.github.com>
2024-06-15 12:18:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // script.js
 
 function getFormValue() {
-    var firstName = document.getElementById("firstName").value;
-    var middleName = document.getElementById("middleName").value;
-	var lastName = document.getElementById("lastName").value;
-
-    var resultMessage =firstName +" "+ middleName+" "+lastName;
-
-    alert(resultMessage);
+    var form = document.getElementById('myForm');
+    
+    // Get the values of the first and last name input fields
+    var firstName = form.elements['firstName'].value;
+    var lastName = form.elements['lastName'].value;
+    
+    // Display the first and last name using alert
+    alert(firstName + " " + lastName);
 	
 }
